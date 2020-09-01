19e5f9de2a787f43787d7ca2b5bf4e13f218d191
Cuong <bdcuong91@gmail.com>
2020-09-01 14:38:46 +0200

    fix bug in home.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,41 +1,43 @@
-import { ToastController } from '@ionic/angular';
-import { Base64ToGallery } from '@ionic-native/base64-to-gallery/ngx';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
-import { Component } from '@angular/core';
+import { ToastController } from "@ionic/angular";
+import { Base64ToGallery } from "@ionic-native/base64-to-gallery/ngx";
+import { BarcodeScanner } from "@ionic-native/barcode-scanner/ngx";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-  qrData = 'https://www.npmjs.com/package/@techiediaries/ngx-qrcode';
+  qrData = "https://google.com";
   scannedCode = null;
-  elementType : 'url' | 'canvas' | 'image' = 'canvas';
+  elementType: "url" | "canvas" | "image" = "canvas";
   constructor(
     private barcodeScanner: BarcodeScanner,
     private base64ToGallery: Base64ToGallery,
     private toastCtrl: ToastController
   ) {}
-  scanCode(){
-    this.barcodeScanner.scan().then(
-      barcodeData => {
-        this.scannedCode = barcodeData;
-      }
-    )
+  scanCode() {
+    this.barcodeScanner.scan().then((barcodeData) => {
+      this.scannedCode = barcodeData.text;
+    });
   }
-  downloadQR(){
-    const canvas = document.querySelector('canvas') as HTMLCanvasElement;
-    const imageData = canvas.toDataURL('image/jpeg').toString();
-    
-    let data = imageData.split(',')[1];
-    console.log('data: ', data);
-    this.base64ToGallery.base64ToGallery(data, {prefix:'_img',mediaScanner:true})
-    .then(async res =>{
-      let toast = await this.toastCtrl.create({
-        header: 'QR Code saved in your Photolibrary'
-      });
-      toast.present();
-    }, err => console.log('err: ', err))
+  downloadQR() {
+    const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+    const imageData = canvas.toDataURL("image/jpeg").toString();
+
+    let data = imageData.split(",")[1];
+    console.log("data: ", data);
+    this.base64ToGallery
+      .base64ToGallery(data, { prefix: "_img", mediaScanner: true })
+      .then(
+        async (res) => {
+          let toast = await this.toastCtrl.create({
+            header: "QR Code saved in your Photolibrary",
+          });
+          toast.present();
+        },
+        (err) => console.log("err: ", err)
+      );
   }
 }
